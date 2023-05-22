4f0a94519f6fdb0d64d95ff469ec0a5b8d5e3d50
muradmecidov <muradmecidov1499@gmail.com>
2023-05-22 11:43:38 +0400

    Basket

---
+++ WebApplication1/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ICollection<Category> categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            bool isExists = await _context.Categories.AnyAsync(c =>
+            c.Name.ToLower().Trim() == category.Name.ToLower().Trim());
+
+            if (isExists)
+            {
+                ModelState.AddModelError("Name", "Category name already exists");
+                return View(category);
+            }
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
